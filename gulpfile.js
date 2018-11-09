f02ec3ba951f3a24d2f9e82f72731d83b961afc4
mob <mob@mikeobrien.net>
2018-11-09 10:07:16 -0500

    Fixed an issue with the build script.

--- gulpfile.js
+++ gulpfile.js
@@ -60,5 +60,8 @@ gulp.task('nuget-package', ['test'], function() {
 });
 
 gulp.task('nuget-push', ['nuget-package'], function() {
-    return Nuget({ apiKey: args.nugetApiKey }).push('*.nupkg');
+    return Nuget({ 
+        apiKey: args.nugetApiKey, 
+        source: ['https://www.nuget.org/api/v2/package'] 
+    }).push('*.nupkg');
 });
